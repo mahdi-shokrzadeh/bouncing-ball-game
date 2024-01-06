a83cca61120682c270a9fe99d8f4466ac5d57cf3
Sina Hashemi <hashemisina6@gmail.com>
2024-01-06 18:36:52 +0330

    added theme 1

--- common/themes.h
+++ common/themes.h
@@ -1,4 +1,19 @@
 #ifndef BOUNCING_BALL_GAME_THEMES_H
 #define BOUNCING_BALL_GAME_THEMES_H
 
+#include <string>
+#include "colors.h"
+
+struct theme {
+    string bg; //background address
+    string music; //music address
+    int MainR, MainG, MainB; //theme main color
+    int SecR, SecG, SecB; //theme second color
+    // to be continued...
+};
+
+const theme th1 = {"", "",
+                   BLUE.r, BLUE.g, BLUE.b,
+                   CYAN.r, CYAN.g, CYAN.b};
+
 #endif //BOUNCING_BALL_GAME_THEMES_H
