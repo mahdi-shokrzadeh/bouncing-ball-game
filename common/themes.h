0ed161f24c59562e4e42d6af32d36bea2b68e1b1
Sina Hashemi <hashemisina6@gmail.com>
2024-01-08 18:25:16 +0330

    added cannon address bar

--- common/themes.h
+++ common/themes.h
@@ -7,12 +7,13 @@
 struct theme {
     string bg; //background address
     string music; //music address
+    string cannon; //cannon image address
     SDL_Color MainColor; //theme main color
     SDL_Color SecColor; //theme second color
     SDL_Color TextColor; //theme text color
     // to be continued...
 };
 
-const theme th1 = {"", "", BLUE, CYAN, WHITE};
+const theme th1 = {"", "", "", BLUE, CYAN, WHITE};
 
 #endif //BOUNCING_BALL_GAME_THEMES_H
