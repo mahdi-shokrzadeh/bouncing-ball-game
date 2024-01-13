4e038e575d6b0bfe8a26e0338efd9386b470e201
Sina Hashemi <hashemisina6@gmail.com>
2024-01-13 11:44:45 +0330

    Included fstream and map

--- main.cpp
+++ main.cpp
@@ -12,11 +12,13 @@
 #include <ctime>
 #include <string>
 #include <vector>
+#include <fstream>
+#include <map>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_mixer.h>
-//include <SDL2/SDL2_gfxPrimitives.h>
+//#include <SDL2/SDL2_gfxPrimitives.h>
 #include <SDL2/SDL2_gfx.h>
 
 using namespace std;
@@ -29,6 +31,7 @@ using namespace std;
 #include "components/structs.h"
 #include "components/window.h"
 #include "components/game.h"
+#include "components/filemanage.h"
 
 
 
