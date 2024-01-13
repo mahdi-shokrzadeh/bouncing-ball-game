c6b5d7cbb59d53a8fd8ae8df454487b1dc005e0d
Sina Hashemi <hashemisina6@gmail.com>
2024-01-13 14:28:58 +0330

    added scoreWriter

--- components/filemanage.h
+++ components/filemanage.h
@@ -4,8 +4,8 @@
 int scoresReader(string a[]) {
 
     ifstream scores;
-    scores.open(".\\assets\\scores.txt");
-    //scores.open("./assets/scores.txt");
+    //scores.open(".\\assets\\scores.txt");
+    scores.open("./assets/scores.txt");
 
     int i = 0;
     if (!scores.good()) {
@@ -20,22 +20,31 @@ int scoresReader(string a[]) {
             i++;
     }
 
-    return i;
-}
-/*
-void scoresWriter() {
+    scores.close();
 
-    ofstream shalgham;
-    shalgham.open("..\\files\\output.txt", ios::app);
-    if (shalgham.good()) {
-        for (int i = 0; i < n; i++)
-            shalgham << a[i] << "\t";
-        shalgham << endl;
-        shalgham.close();
-    } else {
-        cout << "error writing output file...";
+    return i + 1;
+}
+
+void scoresWriter(string name, int score) {
+
+    int n;
+    string a[100];
+    n = scoresReader(a);
+
+    ofstream scores;
+    //scores.open(".\\assets\\scores.txt");
+    scores.open("./assets/scores.txt");
+
+    if (!scores.good()) {
+        cout << "error opening scores file...";
+        return;
     }
 
+    for (int i = 0; i < n; i++) {
+        scores << a[i] << endl;
+    }
+    scores << score << " " << name;
+    scores.close();
 }
-*/
+
 #endif //BOUNCING_BALL_GAME_FILEMANAGE_H
