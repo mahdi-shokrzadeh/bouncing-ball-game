337a24f097340b925e8cdf23e8524395e9a0eb6a
Sina Hashemi <hashemisina6@gmail.com>
2024-01-23 18:10:16 +0330

    fixed type error

--- common/themes.h
+++ common/themes.h
@@ -19,9 +19,9 @@ const theme th1 = {"", "", "", "", "", BLUE, CYAN, WHITE};
 const theme Jungle = {"assets/Jungle/bg.jpg", "assets/Jungle/music.mp3", "assets/Jungle/button.png",
                       "", "assets/Jungle/font.ttf", BROWN, CHOCOLATE, WHITE};
 const theme Ocean = {"assets/Ocean/bg.jpg", "assets/Ocean/music.mp3", "assets/Ocean/button.png",
-                     "", "assets/Ocean/font.ttf", GREEN, FOREST_GREEN, WHITE};
+                     "", "assets/Ocean/font.ttc", GREEN, FOREST_GREEN, WHITE};
 const theme Space = {"assets/Space/bg.jpg", "assets/Space/music.mp3", "assets/Space/button.png",
-                     "", "assets/Space/font.ttf", BLUE, CYAN, WHITE};
+                     "", "assets/Space/font.ttc", BLUE, CYAN, WHITE};
 
 
 void themeChanger(theme newTheme) {
