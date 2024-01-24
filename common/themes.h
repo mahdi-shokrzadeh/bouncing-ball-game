9abeef5deedfc77af054e56d24508d602a3d9a22
Sina Hashemi <hashemisina6@gmail.com>
2024-01-24 21:14:12 +0330

    media added

--- common/themes.h
+++ common/themes.h
@@ -7,6 +7,8 @@ struct theme {
     char bg[64]; //background address
     char music[64]; //music address
     char button[64]; //button address
+    char buttonHover[64]; //button Hover address
+    char buttonQuit[64]; //button Quit address
     char cannon[64]; //cannon image address
     char fontLoc[64]; //font address
     int fontSize;
@@ -16,19 +18,19 @@ struct theme {
     // to be continued...
 };
 
-const theme th1 = {"", "", "", "", "", 50, BLUE, CYAN, WHITE};
+const theme th1 = {"", "", "", "","", "", "", 50, BLUE, CYAN, WHITE};
 const theme Jungle = {"assets/Jungle/bg.jpg", "assets/Jungle/music.mp3",
-                      "assets/Jungle/button.png",
+                      "assets/Jungle/button.png", "assets/Jungle/buttonHover.png", "assets/Jungle/buttonQuit.png",
                       "",
                       "assets/Jungle/font.ttf", 50,
                       BROWN, CHOCOLATE, WHITE};
 const theme Ocean = {"assets/Ocean/bg.jpg", "assets/Ocean/music.mp3",
-                     "assets/Ocean/button.png",
+                     "assets/Ocean/button.png", "assets/Ocean/buttonHover.png", "assets/Ocean/buttonQuit.png",
                      "",
                      "assets/Ocean/font.ttc", 50,
                      GREEN, FOREST_GREEN, WHITE};
 const theme Space = {"assets/Space/bg.jpg", "assets/Space/music.mp3",
-                     "assets/Space/button.png",
+                     "assets/Space/button.png", "assets/Space/buttonHover.png", "assets/Space/buttonQuit.png",
                      "",
                      "assets/Space/font.ttc", 50,
                      BLUE, CYAN, WHITE};
